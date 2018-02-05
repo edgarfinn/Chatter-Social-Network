89c8c7aad43b5ee2dd9c7db10fd718554da9485d
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-05 16:06:06 +0000

    update sample user list to an object

--- sampleDataStructure.js
+++ sampleDataStructure.js
@@ -1,42 +1,43 @@
-const userList = {
-  users: [
-    edgar: {
-      userName: "edgar",
-      posts: [
+var userList = {
+  "users": {
+    "edgar": {
+      "userName": "edgar",
+      "posts": [
         {
-          postedBy: "edgar",
-          contents: "Wow, what an exciting week!",
-          datePosted: "2018-02-01T13:54:30.586Z"
-        },
-        {
-          postedBy: "edgar",
-          contents: "We should do this more often!",
-          datePosted: "2018-02-01T13:55:31.170Z"
-        },
-    ],
-      following: []
-    },
-    john: {
-      userName: "john",
-      posts: [
-        {
-          postedBy: "john",
-          contents: "Cant believe I forgot my keys again!",
-          datePosted: "2018-02-01T14:33:34.533Z"
-        },
+          "postedBy": "edgar",
+          "contents": "Wow, what an exciting week!",
+          "datePosted": "2018-02-01T13:54:30.586Z"
+        }, {
+          "postedBy": "edgar",
+          "contents": "We should do this more often!",
+          "datePosted": "2018-02-01T13:55:31.170Z"
+        }
       ],
-      following: []
+      "following": []
     },
-    bridget: {
-      userName: "bridget",
-      posts: [
+    "john": {
+      "userName": "john",
+      "posts": [
         {
-          postedBy: "bridget",
-          contents: "I've got a spare ticket for the theatre, any takers?",
-          datePosted: "2018-02-01T14:35:16.093Z"
-        },
+          "postedBy": "john",
+          "contents": "Cant believe I forgot my keys again!",
+          "datePosted": "2018-02-01T14:33:34.533Z"
+        }
       ],
-      following: ["edgar", "john"]
+      "following": []
     },
-  ]
+    "bridget": {
+      "userName": "bridget",
+      "posts": [
+        {
+          "postedBy": "bridget",
+          "contents": "I've got a spare ticket for the theatre, any takers?",
+          "datePosted": "2018-02-01T14:35:16.093Z"
+        }
+      ],
+      "following": ["edgar", "john"]
+    }
+  }
 }
+
+module.exports = userList;
