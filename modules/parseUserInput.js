8cff29961ab0c836caeca314015e7b01b75c99a2
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-06 09:33:44 +0000

    #1 initiate actions function matrix with post parameter

--- modules/parseUserInput.js
+++ modules/parseUserInput.js
@@ -1,7 +1,20 @@
 var commandType = require('./IdentifyCommand/identifyCommand.js');
 var userExists = require('./UserExists/userExists.js');
+var userList = require('../Store/dataBase.js');
+var postMessage = require('./PostMessage/postMessage.js');
 
 module.exports = function parseUserInput(command) {
   var components = command.split(" ");
-  console.log(commandType(components));
+
+  var instruction = commandType(components);
+
+  var action = {
+    "post": postMessage
+  }
+
+  action[instruction](components)
+
+
+  // console.log('users after post:',oldUsers);
+
 }
