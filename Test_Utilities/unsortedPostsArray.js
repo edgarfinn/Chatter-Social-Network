58ebc45032dcb94a7c016647a884aa3c94479ffa
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-11 09:38:46 +0000

    #14 sortPosts test utilities initial commit

---
+++ Test_Utilities/unsortedPostsArray.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    "postedBy": "bridget",
+    "contents": "Third post (2011)",
+    "datePosted": "2011-02-01T14:35:16.093Z"
+  }, {
+    "postedBy": "edgar",
+    "contents": "First post (2009)",
+    "datePosted": "2009-01-29T11:54:30.586Z"
+  }, {
+    "postedBy": "john",
+    "contents": "Fourth (last) post (2016)",
+    "datePosted": "2016-02-01T14:33:34.533Z"
+  }, {
+    "postedBy": "edgar",
+    "contents": "Second post (2010)",
+    "datePosted": "2010-02-01T13:55:31.170Z"
+  }
+]
