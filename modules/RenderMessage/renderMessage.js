d86aa2352ed78c2dd551b976e72eb4009c3fa220
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-11 19:34:40 +0000

    #3 remove newline character

--- modules/RenderMessage/renderMessage.js
+++ modules/RenderMessage/renderMessage.js
@@ -4,6 +4,6 @@ module.exports = function(post) {
   var currentDate = new Date();
   var datePosted = new Date(post.datePosted);
   var messageRecency = recency(datePosted,currentDate);
-  var renderedMessage = post.contents + " ("+messageRecency+") \n"
+  var renderedMessage = post.contents + " ("+messageRecency+")"
   return renderedMessage;
 }
