5df764cb967b710e97673268a59ff9c66db060d6
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-11 17:03:20 +0000

    #3 refactor name of readPosts to printPosts

---
+++ modules/PrintPosts/printPosts.js
@@ -0,0 +1,6 @@
+var sortPosts = require('../SortPosts/sortPosts');
+
+module.exports = function(postsArray) {
+  console.log('printing: ', postsArray);
+
+}
--- modules/ReadPosts/readPosts.js
+++
@@ -1,3 +0,0 @@
-module.exports = function(components) {
-  console.log('reading posts', components)
-}
