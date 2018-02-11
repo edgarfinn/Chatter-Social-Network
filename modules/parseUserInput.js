4aaba563c1fa031c8b985740d4c450d89332ccb5
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-11 17:01:36 +0000

    #3 refactor to readTimeline, remove disused module import

--- modules/parseUserInput.js
+++ modules/parseUserInput.js
@@ -2,7 +2,7 @@ var commandType = require('./IdentifyCommand/identifyCommand.js');
 var userExists = require('./UserExists/userExists.js');
 var postMessage = require('./PostMessage/postMessage.js');
 var createNewUser = require('./CreateNewUser/createNewUser.js');
-var readPosts = require('./ReadPosts/readPosts.js');
+var readTimeline = require('./ReadTimeline/readTimeline.js');
 var followUser = require('./FollowUser/followUser.js');
 var readWall = require('./ReadWall/readWall.js');
 
