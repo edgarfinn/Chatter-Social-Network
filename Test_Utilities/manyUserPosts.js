b38e81d30ba0c7565b9053894c575a6fd78b2193
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-11 18:43:38 +0000

    #16 add mock posts DB for renderMessage tests

---
+++ Test_Utilities/manyUserPosts.js
@@ -0,0 +1,45 @@
+{
+  "users": {
+    "edgar": {
+      "userName": "edgar",
+      "posts": [
+        {
+          "postedBy": "edgar",
+          "contents": "Wow what a week!",
+          "datePosted": "2018-02-11T12:31:40.581Z"
+        }, {
+          "postedBy": "edgar",
+          "contents": "we should do this more often!",
+          "datePosted": "2018-02-11T12:32:58.292Z"
+        }
+      ],
+      "following": []
+    },
+    "john": {
+      "userName": "john",
+      "posts": [
+        {
+          "postedBy": "john",
+          "contents": "cant believe I forgot my keys again!!! :(",
+          "datePosted": "2018-02-11T12:37:10.644Z"
+        }
+      ],
+      "following": []
+    },
+    "bridget": {
+      "userName": "bridget",
+      "posts": [
+        {
+          "postedBy": "bridget",
+          "contents": "I've got a spare ticket for the theatre tonight...",
+          "datePosted": "2018-02-11T12:42:30.754Z"
+        }, {
+          "postedBy": "bridget",
+          "contents": "...any takers??",
+          "datePosted": "2018-02-11T12:43:41.076Z"
+        }
+      ],
+      "following": []
+    }
+  }
+}
