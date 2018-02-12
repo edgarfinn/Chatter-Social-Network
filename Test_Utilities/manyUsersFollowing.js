12db0595976aa0c0ac5e021d9a2032e6d5fd0012
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-12 12:48:58 +0000

    #18 manyUsersFollowing.js initial commit

---
+++ Test_Utilities/manyUsersFollowing.js
@@ -0,0 +1,64 @@
+{
+  "users": {
+    "edgar": {
+      "userName": "edgar",
+      "posts": [
+        {
+          "postedBy": "edgar",
+          "contents": "oh what a beautiful mooooorniiiiing",
+          "datePosted": "2018-02-11T20:15:40.908Z"
+        }, {
+          "postedBy": "edgar",
+          "contents": "Wow! eight hour's sleep really makes a difference :D",
+          "datePosted": "2018-02-11T20:16:16.884Z"
+        }, {
+          "postedBy": "edgar",
+          "contents": "I dont even need a coffee",
+          "datePosted": "2018-02-11T20:16:54.470Z"
+        }
+      ],
+      "following": ["john", "sam"]
+    },
+    "john": {
+      "userName": "john",
+      "posts": [
+        {
+          "postedBy": "john",
+          "contents": "ohhh looking fwd to the game tonight!",
+          "datePosted": "2018-02-11T20:15:55.795Z"
+        }, {
+          "postedBy": "john",
+          "contents": "Anyone want to join me at the Nags Head to watch it later?",
+          "datePosted": "2018-02-11T20:17:20.527Z"
+        }
+      ],
+      "following": ["edgar"]
+    },
+    "bridget": {
+      "userName": "bridget",
+      "posts": [
+        {
+          "postedBy": "bridget",
+          "contents": "The Lion King was actually awesome!",
+          "datePosted": "2018-02-11T20:16:45.357Z"
+        }, {
+          "postedBy": "bridget",
+          "contents": "Definitely want to start seeing more theatre now",
+          "datePosted": "2018-02-11T20:17:40.119Z"
+        }
+      ],
+      "following": ["john"]
+    },
+    "sam": {
+      "userName": "sam",
+      "posts": [
+        {
+          "postedBy": "sam",
+          "contents": "Ohh another day another dollar, as they say ;)",
+          "datePosted": "2018-02-11T20:19:17.586Z"
+        }
+      ],
+      "following": ["bridget"]
+    }
+  }
+}
