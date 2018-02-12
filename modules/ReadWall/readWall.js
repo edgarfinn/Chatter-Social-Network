2d6c94b39f1921116331ce945b04659bb6019521
Edgar Dewsbery <edewsbery@gmail.com>
2018-02-12 11:30:30 +0000

    #18 write readWall function

--- modules/ReadWall/readWall.js
+++ modules/ReadWall/readWall.js
@@ -1,3 +1,25 @@
+var prefixUsername = require('../PrefixUsername/prefixUsername');
+var readStore = require('../ReadStore/readStore');
+var printPosts = require('../PrintPosts/PrintPosts');
+
 module.exports = function(components) {
-  console.log('reading wall', components)
+  var filePath = arguments[1] || 'Store/dataBase.js';
+  var userName = components[0];
+  var store = readStore(filePath);
+
+  var profile = store.users[userName];
+  var userPosts = profile.posts;
+  var followedPosts = [];
+
+  profile.following.map(function(followed){
+    return store.users[followed].posts.map(function(post) {
+      followedPosts.push(post)
+    })
+  })
+
+  var wallPosts = userPosts.concat(followedPosts).map(function(post) {
+    return prefixUsername(post)
+  });
+  printPosts(wallPosts);
+  return wallPosts;
 }
---
+++ modules/ReadWall/readWall.test.js
@@ -0,0 +1,46 @@
+var readWall = require('./readWall');
+var readStore = require('../ReadStore/readStore');
+var store = readStore('Test_Utilities/manyUsersFollowing.js');
+var testFilePath = 'Test_Utilities/manyUsersFollowing.js';
+
+var edgarWall = ['edgar', 'wall'];
+var johnWall = ['bridget', 'wall'];
+var bridgetWall = ['john', 'wall'];
+var edgarWallPosts = [
+  {
+    "postedBy": "edgar",
+    "contents": "edgar - oh what a beautiful mooooorniiiiing",
+    "datePosted": "2018-02-11T20:15:40.908Z"
+  }, {
+    "postedBy": "edgar",
+    "contents": "edgar - Wow! eight hour's sleep really makes a difference :D",
+    "datePosted": "2018-02-11T20:16:16.884Z"
+  }, {
+    "postedBy": "edgar",
+    "contents": "edgar - I dont even need a coffee",
+    "datePosted": "2018-02-11T20:16:54.470Z"
+  }, {
+    "postedBy": "john",
+    "contents": "john - ohhh looking fwd to the game tonight!",
+    "datePosted": "2018-02-11T20:15:55.795Z"
+  }, {
+    "postedBy": "john",
+    "contents": "john - Anyone want to join me at the Nags Head to watch it later?",
+    "datePosted": "2018-02-11T20:17:20.527Z"
+  }, {
+    "postedBy": "sam",
+    "contents": "sam - Ohh another day another dollar, as they say ;)",
+    "datePosted": "2018-02-11T20:19:17.586Z"
+  }
+]
+
+// followedPosts returns expected array of posts
+test('readWall returns all posts by user followed', function() {
+  expect(readWall(edgarWall, testFilePath)).toEqual(
+    expect.arrayContaining(edgarWallPosts)
+  )
+})
+
+// followedPosts contains all posts by users followed users
+
+// all posts are prefixed with postedBy userName
